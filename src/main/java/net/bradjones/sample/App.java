4e1884f43e0c295fdf2204b60a9d17ad9966561b
daniel-77 <daniel-77@users.noreply.github.com>
2017-04-15 18:51:46 +0800

    add sample project

---
+++ src/main/java/net/bradjones/sample/App.java
@@ -0,0 +1,13 @@
+package net.bradjones.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
